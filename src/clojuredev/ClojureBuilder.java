9ae95f775fb08d9d63cbe07770086c2a1c97a0a8
casey.marshall <none@none>
2008-10-13 03:48:09 +0000

    Added updated builder, launch shortcut.
    --HG--
    extra : convert_revision : svn%3A951d75ee-2d4e-0410-a0c2-87c8fdd8ec4c/clojuredev/trunk%4016

--- src/clojuredev/ClojureBuilder.java
+++ src/clojuredev/ClojureBuilder.java
@@ -2,6 +2,7 @@ package clojuredev;
 
 import java.util.Map;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
@@ -15,25 +16,28 @@ public class ClojureBuilder extends IncrementalProjectBuilder {
 	@Override
 	protected IProject[] build(int kind, Map args, IProgressMonitor monitor)
 			throws CoreException {
+		System.out.println("build:" + kind + " args:" + args);
 		IResourceDelta delta = getDelta(getProject());
 		incrementalBuild(delta, monitor);
 		return null;
 	}
 
 	private void incrementalBuild(IResourceDelta delta, IProgressMonitor monitor) {
-		clojure.lang.Compiler compiler = new clojure.lang.Compiler();
-		monitor.beginTask("Evaluating " + delta.getFullPath().lastSegment(), IProgressMonitor.UNKNOWN);
-		try {
-			Object result = compiler.loadFile(delta.getFullPath().toOSString());
-			// TODO: get warnings?
-			System.out.println("eval " + delta.getFullPath().lastSegment() + ": " + result.toString());
-			monitor.worked(1);
-			monitor.done();
-		}
-		catch (Exception e) {
-			// TODO: add to compiler errors
-			System.out.println("eval " + delta.getFullPath().lastSegment() + " failed:");
-			e.printStackTrace(System.out);
+		if (delta != null && delta.getResource() instanceof IFile) {
+			IFile deltaFile = (IFile)delta.getResource();
+			monitor.beginTask("Evaluating " + delta.getFullPath().lastSegment(), IProgressMonitor.UNKNOWN);
+			try {
+				Object result = clojure.lang.Compiler.loadFile(deltaFile.getLocation().toFile().toString());
+				// TODO: get warnings?
+				System.out.println("eval " + delta.getFullPath().lastSegment() + ": " + result.toString());
+				monitor.worked(1);
+				monitor.done();
+			}
+			catch (Exception e) {
+				// TODO: add to compiler errors
+				System.out.println("eval " + delta.getFullPath().lastSegment() + " failed:");
+				e.printStackTrace(System.out);
+			}
 		}
 	}
 
--- src/clojuredev/launching/ClojureLaunchShortcut.java
+++ src/clojuredev/launching/ClojureLaunchShortcut.java
@@ -1,21 +1,36 @@
 package clojuredev.launching;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.debug.ui.ILaunchShortcut;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
 
 public class ClojureLaunchShortcut implements ILaunchShortcut {
 
 	@Override
 	public void launch(ISelection selection, String mode) {
-		// TODO Auto-generated method stub
-
+		System.out.println("launch:" + selection.toString());
 	}
 
 	@Override
 	public void launch(IEditorPart editor, String mode) {
-		// TODO Auto-generated method stub
-
+		IEditorInput input = editor.getEditorInput();
+		if (input instanceof FileEditorInput) {
+			IFile file = ((FileEditorInput)input).getFile();
+			
+			try {
+				Object result = clojure.lang.Compiler.loadFile(file.getLocation().toFile().toString());
+				if (result != null) {
+					System.out.println(result.toString());
+				}
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
