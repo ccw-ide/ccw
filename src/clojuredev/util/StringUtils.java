76409069a4088e62dca3d128a1604a9a5b6de52b
Laurent Petit <laurent.petit@gmail.com>
2009-08-11 22:34:52 +0200

    Renaming of clojuredev as Counterclockwise

--- src/clojuredev/util/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package clojuredev.util;
-
-public final class StringUtils {
-	private StringUtils () { /* Not intended to be instanciated */ }
-	public static boolean isEmpty(String s) {
-		return s == null
-			|| s.length() == 0;
-	}
-}
