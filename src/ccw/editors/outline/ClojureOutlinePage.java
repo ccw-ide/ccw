d73e94009c20c87ed97d3bae8a7cb44e6773ef19
Manuel Woelker <manuel.woelker+github@gmail.com>
2009-10-08 19:55:42 +0200

    Added epl header to ClojureOutlinePage

--- src/ccw/editors/outline/ClojureOutlinePage.java
+++ src/ccw/editors/outline/ClojureOutlinePage.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Manuel Woelker.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: 
+ *    Manuel Woelker - initial prototype implementation
+ *******************************************************************************/
 package ccw.editors.outline;
 
 import java.io.StringReader;
