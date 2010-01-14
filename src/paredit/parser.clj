8ca5169911d9a44a1788a1f77b399637ea0cd49f
Laurent Petit <laurent.petit@gmail.com>
2010-01-15 00:24:13 +0100

    introduced literal characters handling to the parser. Correct handling of them by :paredit-open-* functions

--- src/paredit/parser.clj
+++ src/paredit/parser.clj
@@ -1,3 +1,4 @@
+; note : hiredman's reader http://github.com/hiredman/clojure/blob/readerII/src/clj/clojure/reader.clj#L516
 (ns paredit.parser
 	(:use clojure.test))
 
@@ -7,9 +8,49 @@
 (def *opening-brackets* (set (keys *brackets*)))
 (def *closing-brackets* (set (vals *brackets*)))
 
+(defn 
+  start-like
+  "returns true if s1 is a prefix of s2 or s2 is a prefix of s1.
+   Examples:
+   (= true (starts-like \"bar and foo\" \"bar\"))
+   (= true (starts-like \"bar\" \"bar and foo\"))
+   (= false (starts-like \"bar\" \"baz\"))
+   (= true (starts-like \"bar\" \"bar\"))
+   (= true (starts-like \"ba\" \"bar\"))
+   (= true (starts-like \"bar\" \"ba\"))
+   (= true (starts-like \"b\" \"bar\"))
+   (= true (starts-like \"bar\" \"b\"))
+   (= true (starts-like \"\" \"bar\"))
+   (= true (starts-like \"bar\" \"\"))"
+  [#^String s1 #^String s2]
+    (or (.startsWith s1 s2) (.startsWith s2 s1))
+    #_(.startsWith s1 (.substring s2 0 (min (.length s2) (.length s1)))))
+
+(deftest test-starts-like
+  (testing "starts-like"
+    (are [expected s1 s2]  (= expected (starts-like s1 s2))
+      true "bar and foo" "bar"
+      true "bar" "bar and foo"
+
+      false "bar" "baz"
+      false "baz" "bar"
+
+      true "bar" "bar"
+
+      true "ba" "bar"
+      true "bar" "ba"
+
+      true "bar" "b"
+      true "bar" "b"
+
+      true "bar" ""
+      true "" "bar"
+      )))
+      
+
 "\"parser state \" datastructure: (not really a parser currently, contains just the stack of containing balanced expression up to
  the top level)
- { :parents [ {:type <[ or ( or { or \"> :line :offset :col} ... ]
+ { :parents [ {:type <[ or ( or { or \" or \\> :line :offset :col} ... ]
    :offset current-offset :line current-line :col current-col}"
 
 (defn parse 
@@ -25,67 +66,101 @@
 	      (if (>= offset stop-offset)
 	        { :parents parents :offset offset :line line :col col }
 	        (let [c (.charAt text offset)] ; c: current char
-	          (cond
-	            (*opening-brackets* c)
-	              (cond 
-	                (= \" (-> parents peek :type))
-	                  (recur (inc offset) line (inc col) parents)
-	                (= \; (-> parents peek :type))
-	                  (recur (inc offset) line (inc col) parents)
-	                :else
-		                (recur 
-		                  (inc offset) line (inc col)
-		                  (conj parents {:type c :offset offset :line line :col col})))
-	            (*closing-brackets* c)
-	              (cond 
-	                (= \" (-> parents peek :type))
-	                  (recur (inc offset) line (inc col) parents)
-	                (= \; (-> parents peek :type))
-	                  (recur (inc offset) line (inc col) parents)
-	                :else
-		                (recur 
-		                  (inc offset) line (inc col)
-		                  (pop parents)))
-	            (= (first "\r") c)
-	              (recur (inc offset) line col parents) ; we do not increment the column    
-	            (= \newline c)
-	              (cond 
-	                (= \" (-> parents peek :type))
-		                (recur (inc offset) (inc line) (int 0) parents)
-	                (= \; (-> parents peek :type))
-	                  ; we take care of going out of comments, if we are in comments !
-		                (recur (inc offset) (inc line) (int 0) (pop parents))
-	                :else
-		                (recur (inc offset) (inc line) (int 0) parents))
-	            #_(#{\space \tab \,} c)
-	              #_(cond 
-	                (= \" (-> parents peek :type))
-	                (= \; (-> parents peek :type))
-	                :else
-	              )
-	            (= \" c)
-	              (cond 
-	                (= \" (-> parents peek :type))
-	                  (if (= offset 0)
-	                    (recur (inc offset) line (inc col) (conj parents {:type c :line line :col col :offset offset}))
-	                    (if (= \\ (.charAt text (dec offset)))
-	                      (recur (inc offset) line (inc col) parents)
-	                      (recur (inc offset) line (inc col) (pop parents))))
-	                (= \; (-> parents peek :type))
-	                  (recur (inc offset) line (inc col) parents)
-	                :else
-	                  (recur (inc offset) line (inc col) (conj parents {:type c :line line :col col :offset offset})))
-	            (= \; c)
-	              (cond 
-	                (= \" (-> parents peek :type))
-	                  (recur (inc offset) line (inc col) parents)
-	                (= \; (-> parents peek :type))
-	                  (recur (inc offset) line (inc col) parents)
-	                :else
-	                  (recur (inc offset) line (inc col) (conj parents {:type c :offset offset :col col :line line})))
-	            :else
-	              (recur (inc offset) line (inc col) parents)))))))
-
+	          (condp = (-> parents peek :type)
+	            \" 
+                (cond
+                  (= \newline c)
+                    (recur (inc offset) (inc line) (int 0) parents)
+                  (= \" c)
+                    (if (= offset 0)
+                      (recur (inc offset) line (inc col) (conj parents {:type c :line line :col col :offset offset}))
+                      (if (= \\ (.charAt text (dec offset)))
+                        (recur (inc offset) line (inc col) parents)
+                        (recur (inc offset) line (inc col) (pop parents))))
+                  :else
+                    (recur (inc offset) line (inc col) parents))
+              \;	          
+                (cond
+                  (= \newline c)
+                    (recur (inc offset) (inc line) (int 0) (pop parents))
+                  :else
+                    (recur (inc offset) line (inc col) parents))
+              \\
+                (cond   ; leaved as a template if new cases have to be handled
+                  #_(*opening-brackets* c)
+                  #_(*closing-brackets* c)
+                  #_(= (first "\r") c)
+                  #_(= \newline c)
+                  #_(= \" c)
+                  #_(= \; c)
+                  ; TODO refactor the following stuff. or keep for performance ? (bleh)
+                  (starts-like (.substring text (-> parents peek :offset)) "\\newline")
+                    (if (< (inc (- offset (-> parents peek :offset))) (.length "\\newline"))
+                      (recur (inc offset) line (inc col) parents)
+                      (recur (inc offset) line (inc col) (pop parents)))
+                  (starts-like (.substring text (-> parents peek :offset)) "\\tab")
+                    (if (< (inc (- offset (-> parents peek :offset))) (.length "\\tab"))
+                      (recur (inc offset) line (inc col) parents)
+                      (recur (inc offset) line (inc col) (pop parents)))
+                  (starts-like (.substring text (-> parents peek :offset)) "\\space")
+                    (if (< (inc (- offset (-> parents peek :offset))) (.length "\\space"))
+                      (recur (inc offset) line (inc col) parents)
+                      (recur (inc offset) line (inc col) (pop parents)))
+                  (starts-like (.substring text (-> parents peek :offset)) "\\backspace")
+                    (if (< (inc (- offset (-> parents peek :offset))) (.length "\\backspace"))
+                      (recur (inc offset) line (inc col) parents)
+                      (recur (inc offset) line (inc col) (pop parents)))
+                  (starts-like (.substring text (-> parents peek :offset)) "\\formfeed")
+                    (if (< (inc (- offset (-> parents peek :offset))) (.length "\\formfeed"))
+                      (recur (inc offset) line (inc col) parents)
+                      (recur (inc offset) line (inc col) (pop parents)))
+                  (starts-like (.substring text (-> parents peek :offset)) "\\return")
+                    (if (< (inc (- offset (-> parents peek :offset))) (.length "\\return"))
+                      (recur (inc offset) line (inc col) parents)
+                      (recur (inc offset) line (inc col) (pop parents)))
+                  ; TODO I don't like the fact the next two conditions on \r and \newline are repeated from the default case
+                  ;      clearly a level of indirection is missing regarding the update of line, offset and col
+                  (= (first "\r") c)
+                    (recur (inc offset) line col parents) ; we do not increment the column    
+                  (= \newline c)
+                    (recur (inc offset) (inc line) (int 0) parents)
+                  :else
+                    (recur (inc offset) line (inc col) (pop parents)))
+              ; last falling case: we are in plain code, neither in a string, regexp or a comment
+              (cond
+                (*opening-brackets* c)
+                  (recur 
+                    (inc offset) line (inc col)
+                    (conj parents {:type c :offset offset :line line :col col}))
+                (*closing-brackets* c)
+                  (recur 
+                    (inc offset) line (inc col)
+                    (pop parents))
+                (= (first "\r") c)
+                  (recur (inc offset) line col parents) ; we do not increment the column    
+                (= \newline c)
+                  (recur (inc offset) (inc line) (int 0) parents)
+                (= \" c)
+                  (recur (inc offset) line (inc col) (conj parents {:type c :offset offset :line line :col col }))
+                (= \; c)
+                  (recur (inc offset) line (inc col) (conj parents {:type c :offset offset :line line :col col}))
+                (= \\ c)
+                  (recur (inc offset) line (inc col) (conj parents {:type c :offset offset :line line :col col}))
+                :else
+                  (recur (inc offset) line (inc col) parents))
+                
+                #_(cond   ; leaved as a template if new cases have to be handled
+                  (*opening-brackets* c)
+                  (*closing-brackets* c)
+                  (= (first "\r") c)
+                  (= \newline c)
+                  (= \" c)
+                  (= \; c)
+                  (= \\ c)
+                  :else
+                    (recur (inc offset) line (inc col) parents))))))))
+	          
+(comment
 (defn parse-lib 
   ([lib] (parse-lib lib false))
   ([lib show?]
@@ -96,66 +171,6 @@
   (parse-lib "core" true))
 (defn parse-set []
 	(parse-lib "set" true))
-
-#_(deftest test-format-code
-  (testing "parens combinatorics"
-    (are [x y] (= x (format-code y))
-"" ""
-"" nil
-"()" "()"
-"[]" "[]"
-"{}" "{}"
-"#{}" "#{}"
-#_"()" #_"()   "
-#_"()" #_"(  )"
-#_"()" #_"   ()"
-#_"()" #_"  (   , ,  \t)"
-#_"(())" #_"(())"
-#_"(())" #_"   (   (   )  )"
-
-#_"
-(
-  ())" 
-#_"
-(
-())"
-
-#_"(\n  ())" "(\n         (       ))"
-
-#_"
-((
-   ()))"         
-#_"
-((
-()))"
-
-#_"
-(
-  ((
-     ())))" 
-#_"
-   (\n(       (\n( ) ) )   ) ,"
-
-#_"
-((
- ))"
-#_"
-((
-))"
-
-#_"
-[[
- ]]"
-#_"
-[[
-]]"
-
-#_"abc"
-#_"abc"
-
-#_"(def toto [] (println 123))"
-#_"(def toto [] (println 123))"
-    )))
-(def rt run-tests)
+)
 
   
