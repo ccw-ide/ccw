bb3cb77e3a97781dbe962b32236037f11af55748
Laurent Petit <laurent.petit@gmail.com>
2010-01-23 23:28:37 +0100

    adapted to the version of paredit.clj which emits text deltas. Better handling of undos, not showing full text selection anymore

--- ccw.core/src/ccw/editors/antlrbased/PareditAutoEditStrategy.clj
+++ ccw.core/src/ccw/editors/antlrbased/PareditAutoEditStrategy.clj
@@ -27,9 +27,9 @@
                           {:text (.get document) 
                            :offset (.offset command) 
                            :length 0})]
-      (println "result:" result)
-      (set! (.offset command) 0)
-      (set! (.length command) (.length (.get document)))
-      (set! (.text command) (:text result))
+      #_(println "result:" result)
+      (set! (.offset command) (-> result :modifs first :offset))
+      (set! (.length command) (-> result :modifs first :length))
+      (set! (.text command) (-> result :modifs first :text))
       (set! (.shiftsCaret command) false)
       (set! (.caretOffset command) (:offset result)))))
