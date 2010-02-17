549010f5f9c2399ac1a2d67e16851f0403684546
Miaubiz <miaubiz@gmail.com>
2010-02-17 10:57:14 +0200

    renamed files

---
+++ ccw.core/src/ccw/editors/antlrbased/OpenDeclarationAction.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Tuomas KARKKAINEN.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: 
+ *    Tuomas KARKKAINEN - initial API and implementation
+ *******************************************************************************/
+package ccw.editors.antlrbased;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.jface.action.Action;
+
+import ccw.ClojureCore;
+import ccw.debug.ClojureClient;
+import clojure.lang.PersistentArrayMap;
+
+public class OpenDeclarationAction extends Action {
+    public final static String ID = "OpenDeclarationAction"; //$NON-NLS-1$
+    private final AntlrBasedClojureEditor editor;
+
+    public OpenDeclarationAction(AntlrBasedClojureEditor editor) {
+        this.editor = editor;
+    }
+
+    @Override
+    public void run() {
+        int caretOffset = editor.sourceViewer().getTextWidget().getCaretOffset();
+        Tokens tokens = new Tokens(editor.getDocument(), caretOffset);
+        tokens.tokenAtCaret();
+        String tokenContents = tokens.tokenContents();
+        List<String> split = Arrays.asList(tokenContents.split("/"));
+        String symbol = tokenContents;
+        String declaringNamespace = editor.getDeclaringNamespace();
+        String namespace = null;
+        if (split.size() == 2) {
+            symbol = split.get(1);
+            namespace = split.get(0);
+        }
+        String command = String.format("(ccw.debug.serverrepl/find-symbol \"%s\" \"%s\" \"%s\")", symbol, declaringNamespace, namespace);
+        ClojureClient clojure = ClojureClient.newClientForActiveRepl();
+        if (clojure == null) {
+            editor.setStatusLineErrorMessage(ClojureEditorMessages.You_need_a_running_repl);
+            return;
+        }
+        PersistentArrayMap result2 = (PersistentArrayMap) clojure.remoteLoadRead(command);
+        List<String> result = (List<String>) result2.get("response");
+        if (result == null || result.isEmpty() || result2.get("response-type").equals(-1)) {
+            editor.setStatusLineErrorMessage(ClojureEditorMessages.Cannot_find_declaration);
+            return;
+        }
+        String file = result.get(0);
+        Integer line = Integer.valueOf(result.get(1));
+        String ns = result.get(3);
+        if (file.endsWith(editor.getPartName())) {
+            ClojureCore.gotoEditorLine(editor, line);
+        } else {
+            ClojureCore.openInEditor(ns, file, line);
+        }
+    }
+}
