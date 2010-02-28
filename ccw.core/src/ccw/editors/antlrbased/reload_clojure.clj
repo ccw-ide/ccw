b2fd9d36576dd5592de603cadd19dd0de8936a29
miaubiz <miaubiz@gmail.com>
2010-02-28 10:30:53 +0200

    add banner

--- ccw.core/src/ccw/editors/antlrbased/reload_clojure.clj
+++ ccw.core/src/ccw/editors/antlrbased/reload_clojure.clj
@@ -1,3 +1,14 @@
+; *******************************************************************************
+; * Copyright (c) 2010 Tuomas KARKKAINEN.
+; * All rights reserved. This program and the accompanying materials
+; * are made available under the terms of the Eclipse Public License v1.0
+; * which accompanies this distribution, and is available at
+; * http://www.eclipse.org/legal/epl-v10.html
+; *
+; * Contributors: 
+; *    Tuomas KARKKAINEN - initial API and implementation
+; *******************************************************************************/
+
 (ns ccw.editors.antlrbased.reload-clojure
   (:gen-class
     :init init
