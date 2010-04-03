9dd7b6236138df514cda72b644daf99f3d2d629f
Laurent Petit <laurent.petit@gmail.com>
2010-04-03 14:11:51 +0200

    try to better handle project/clean problems on windows

--- ccw.core/src/ccw/builder/ClojureBuilder.java
+++ ccw.core/src/ccw/builder/ClojureBuilder.java
@@ -163,14 +163,14 @@ public class ClojureBuilder extends IncrementalProjectBuilder {
     	
     	try {
     		getClassesFolder().delete(true, monitor);
+	    	if (!getClassesFolder().exists()) {
+	    		getClassesFolder().create(true, true, monitor);
+	    	}
+	        getClassesFolder().refreshLocal(IResource.DEPTH_INFINITE, new SubProgressMonitor(monitor, 0));
+	        getClassesFolder().setDerived(true); //, monitor); removed monitor argument, probably a 3.5/3.6 only stuff
     	} catch (CoreException e) {
-    		CCWPlugin.logError("Unable to clean classes folder", e);
+    		CCWPlugin.logError("Unable to correctly clean classes folder", e);
     	}
-    	if (!getClassesFolder().exists()) {
-    		getClassesFolder().create(true, true, monitor);
-    	}
-        getClassesFolder().refreshLocal(IResource.DEPTH_INFINITE, new SubProgressMonitor(monitor, 0));
-        getClassesFolder().setDerived(true); //, monitor); removed monitor argument, probably a 3.5/3.6 only stuff
 
         deleteMarkers();
     }
