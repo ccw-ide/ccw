781cd08342c5ee5bb489fc519282f1042a22ede9
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2010-04-28 22:51:59 +0200

    Add selection logic for buttons.

--- ccw.support.examples/src/ccw/support/labrepl/wizards/LabreplCreateProjectPage.clj
+++ ccw.support.examples/src/ccw/support/labrepl/wizards/LabreplCreateProjectPage.clj
@@ -12,7 +12,8 @@
   (:import
     [org.eclipse.swt SWT]
     [org.eclipse.swt.widgets Button Composite]
-    [org.eclipse.swt.layout GridLayout GridData])
+    [org.eclipse.swt.layout GridLayout GridData]
+    [org.eclipse.swt.events SelectionAdapter] )
   (:gen-class
    :extends org.eclipse.ui.dialogs.WizardNewProjectCreationPage
    :exposes-methods {createControl super-createControl}
@@ -33,15 +34,27 @@
   (let
     [composite (Composite. (.getControl this) SWT/NONE)
      layout (GridLayout.)]
+    
     (doto composite
       (.setLayout layout)
       (.setLayoutData (GridData. (bit-or GridData/VERTICAL_ALIGN_FILL GridData/FILL_HORIZONTAL))))
-    (let [run-lein-deps-button (Button. composite SWT/CHECK)]
+    
+    ; Create the check buttons for running "lein deps" and for starting up
+    ; the REPL and for displaying the Labrepl start page.
+    ; The latter button only is enabled if the "lein deps" button is checked.
+    (let [run-lein-deps-button (Button. composite SWT/CHECK)
+          run-repl-button (Button. composite SWT/CHECK)]
       (doto run-lein-deps-button
         (.setText "Run \"lein deps\" to download dependencies")
-        (.setSelection  true)))
-    (let [run-repl-button (Button. composite SWT/CHECK)]
+        (.setSelection  true)
+        (.addSelectionListener 
+          (proxy [SelectionAdapter] []
+            (widgetSelected [_]
+              (.setEnabled run-repl-button (.getSelection run-lein-deps-button))))))
+
       (doto run-repl-button
         (.setText "Run REPL and open web page")
-        (.setSelection  true)))
-    (.validatePage this)))
+        (.setSelection  true))
+      (dosync (alter (.state this) assoc :run-repl-button run-repl-button :run-lein-deps-button run-lein-deps-button))))
+  
+  (.validatePage this))
