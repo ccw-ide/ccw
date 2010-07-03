79e8e995b626f1103bcdbc1f48aef1a922997093
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2010-07-03 21:34:57 +0200

    Add a compile.clj to the leiningen plugin to bootstrap it without leiningen.

---
+++ ccw.support.leiningen/src/compile.clj
@@ -0,0 +1,33 @@
+;*******************************************************************************
+;* Copyright (c) 2010 Stephan Muehlstrasser.
+;* All rights reserved. This program and the accompanying materials
+;* are made available under the terms of the Eclipse Public License v1.0
+;* which accompanies this distribution, and is available at
+;* http://www.eclipse.org/legal/epl-v10.html
+;*
+;* Contributors: 
+;*    Stephan Muehlstrasser - initial API and implementation
+;*******************************************************************************/
+(ns compile)
+
+(defn all []
+  (dorun   
+    (map
+      compile
+      [
+       'lancet
+       'leiningen.clean
+       'leiningen.core  
+       'leiningen.help     
+       'leiningen.jar  
+       'leiningen.pom   
+       'leiningen.uberjar  
+       'leiningen.version
+       'leiningen.compile  
+       'leiningen.deps  
+       'leiningen.install  
+       'leiningen.new  
+       'leiningen.test  
+       'leiningen.upgrade
+       ])))
+
