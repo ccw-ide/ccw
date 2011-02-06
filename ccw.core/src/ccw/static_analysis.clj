f39207b40efadc2093fbbebaa99cc8e7b52551f8
Laurent Petit <laurent.petit@gmail.com>
2011-02-06 14:56:40 +0100

    enhanced ns detection: if there's a top level in-ns, it's used

--- ccw.core/src/ccw/static_analysis.clj
+++ ccw.core/src/ccw/static_analysis.clj
@@ -1,5 +1,13 @@
 (ns ccw.static-analysis
   (:require [paredit.parser :as p]))
 
-(defn find-namespace 
-  [tree] (p/sym-name (p/form (first (p/call-args (some #(p/call-of % "ns") (p/code-children tree)))))))
+(defn find-namespace [tree] 
+  (p/sym-name 
+    (p/form 
+      (first 
+        (p/call-args 
+          (some 
+            #(or 
+               (p/call-of % "ns") 
+               (p/call-of % "in-ns"))
+            (p/code-children tree)))))))
