e71263670666a1869e0d97bfa25f0a43f1332263
Chas Emerick <chas@cemerick.com>
2012-08-15 07:21:48 -0400

    Ensure that all REPLs open in the position to which the last REPL was moved; fixes 404

--- ccw.core/src/java/ccw/repl/REPLView.java
+++ ccw.core/src/java/ccw/repl/REPLView.java
@@ -5,7 +5,7 @@ import java.net.ConnectException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
+import java.util.TreeSet;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 
@@ -103,6 +103,39 @@ public class REPLView extends ViewPart implements IAdaptable {
             CCWPlugin.logError("Could not initialize view helpers.", e);
         }
     }
+
+    /**
+     * This misery around secondary IDs is due to the fact that:
+     * eclipse really, really wants views to be pre-defined; while you can have a view type that
+     * has multiple instances, doing so requires having *stable* secondary IDs in order for the
+     * IDE to retain positioning and size preferences.
+     * We could just use UUIDs to identify different REPL views, but that means no position info
+     * will be retained.  We could use nREPL URLs, but those are almost as bad as UUIDs insofar as
+     * the autoselected ports are hardly ever used twice.
+     * Try as I might, I cannot find a way to get a view to (a) be displayed in a particular place
+     * in the IDE (short of defining a Clojure perspective!) or (b) be notified of when a REPL
+     * view is moved to begin with.
+     * 
+     * This approach will result in a user "training" the IDE where to put REPL views
+     * (with seemingly intransigent or arbitrary behaviour to start); eventually, REPLs will
+     * all end up displaying in the desired location.
+     */
+    private static Set<String> SECONDARY_VIEW_IDS = new TreeSet<String>() {{
+       // no one will create more than 1000 REPLs at a time, right? :-P
+       for (int i = 0; i < 1000; i++) add(String.format("%03d", i)); 
+    }};
+    private static String getSecondaryId () {
+        synchronized (SECONDARY_VIEW_IDS) {
+            String id = SECONDARY_VIEW_IDS.iterator().next();
+            SECONDARY_VIEW_IDS.remove(id);
+            return id;
+        }
+    }
+    private static void releaseSecondaryId (String id) {
+        synchronized (SECONDARY_VIEW_IDS) {
+            SECONDARY_VIEW_IDS.add(id);
+        }
+    }
     
     private static final Keyword inputExprLogType = Keyword.intern("in-expr");
     private static final Pattern boostIndent = Pattern.compile("^", Pattern.MULTILINE);
@@ -134,6 +167,7 @@ public class REPLView extends ViewPart implements IAdaptable {
     private Map<String, Object> describeInfo;
     private IFn evalExpression;
     private String sessionId;
+    private String secondaryId;
     
     /* function implementing load previous/next command from history into input area */
     private IFn historyActionFn;
@@ -148,7 +182,7 @@ public class REPLView extends ViewPart implements IAdaptable {
     private SourceViewerDecorationSupport fSourceViewerDecorationSupport;
 	private StatusLineContributionItem structuralEditionModeStatusContributionItem;
     
-    public REPLView () {}
+    public REPLView () {}    
     
     private void resetFont () {
         Font font= JFaceResources.getTextFont();
@@ -300,9 +334,12 @@ public class REPLView extends ViewPart implements IAdaptable {
     }
     
     public static REPLView connect (String url, IConsole console, ILaunch launch) throws Exception {
-        REPLView repl = (REPLView)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(VIEW_ID,
-                UUID.randomUUID().toString(),
+        String secondaryId;
+        REPLView repl = (REPLView)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(
+                VIEW_ID,
+                secondaryId = getSecondaryId(),
                 IWorkbenchPage.VIEW_ACTIVATE);
+        repl.secondaryId = secondaryId;
         repl.console = console;
         repl.launch = launch;
         return repl.configure(url) ? repl : null;
@@ -648,6 +685,9 @@ public class REPLView extends ViewPart implements IAdaptable {
     @Override
     public void dispose() {
         super.dispose();
+
+        releaseSecondaryId(secondaryId);
+        
         fSourceViewerDecorationSupport = (SourceViewerDecorationSupport) editorSupport._("disposeSourceViewerDecorationSupport",
         		fSourceViewerDecorationSupport);
         try {
