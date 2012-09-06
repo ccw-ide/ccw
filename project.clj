dcca7fa15f00231450afe7b7d299deea1e82559e
Tom Hickey <thickey@gmail.com>
2012-09-06 10:45:55 -0400

    Added core.incubator dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject org.lpetit/paredit.clj "0.17.0.STABLE001"
+(defproject org.lpetit/paredit.clj "0.18.0"
   :description "paredit in clojure, tailored for clojure"
   :url "https://github.com/laurentpetit/paredit.clj"
   :license {:name "Eclipse Public License 1.0"
             :url "http://opensource.org/licenses/eclipse-1.0.php"
             :distribution "repo"}
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.incubator "0.1.1"]
                  [org.lpetit/net.cgrand.parsley "0.0.12.STABLE001"]
                  [org.lpetit/net.cgrand.regex "0.0.4.STABLE001"]])
