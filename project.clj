75ccce852c9681ae6d312825c01ee54ad425ecd5
Tom Hickey <thickey@gmail.com>
2012-09-06 09:38:33 -0400

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject org.lpetit/paredit.clj "0.17.0.STABLE001"
+  :description "paredit in clojure, tailored for clojure"
+  :url "https://github.com/laurentpetit/paredit.clj"
+  :license {:name "Eclipse Public License 1.0"
+            :url "http://opensource.org/licenses/eclipse-1.0.php"
+            :distribution "repo"}
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.lpetit/net.cgrand.parsley "0.0.12.STABLE001"]
+                 [org.lpetit/net.cgrand.regex "0.0.4.STABLE001"]])
