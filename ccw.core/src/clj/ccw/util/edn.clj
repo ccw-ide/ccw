0ac00daeacfa992775e22ac190e0a7fa9614bb08
Timo Mihaljov <timo@mihaljov.info>
2013-07-04 11:36:53 +0200

    ccw.util.edn: Wrap tests in deftest

--- ccw.core/src/clj/ccw/util/edn.clj
+++ ccw.core/src/clj/ccw/util/edn.clj
@@ -1,6 +1,6 @@
 (ns ccw.util.edn
   (:require [clojure.edn :as edn]
-            [clojure.test :refer [testing is]]))
+            [clojure.test :refer [deftest testing is]]))
 
 (defn read-vector
   "Reads a vector from the EDN string s. Returns an empty vector if s does not
@@ -14,10 +14,11 @@
       x
       [])))
 
-(testing "read-vector"
-  (testing "returns the vector in the input string"
-    (is (= [123 456] (read-vector "[123 456]"))))
-  (testing "returns an empty vector if input is invalid EDN"
-    (is (= [] (read-vector "[123"))))
-  (testing "returns an empty vector if input is not a vector"
-    (is (= [] (read-vector "123")))))
+(deftest test-read-vector
+  (testing "read-vector"
+    (testing "returns the vector in the input string"
+      (is (= [123 456] (read-vector "[123 456]"))))
+    (testing "returns an empty vector if the input is invalid EDN"
+      (is (= [] (read-vector "[123"))))
+    (testing "returns an empty vector if the input is not a vector"
+      (is (= [] (read-vector "123"))))))
