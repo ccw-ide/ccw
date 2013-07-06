05a0c68ef5e0ff817b77ec6cedf365d0f2333732
Laurent Petit <laurent.petit@gmail.com>
2013-07-06 10:20:40 +0200

    Fix Usage error for scripts/set-version.sh

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-test $1 || ( echo "usage: set-version.sh <new-version>"; exit 1; )
+if [[ "" == $1 ]]; then
+    echo "usage: set-version.sh <new-version>"
+    exit 1
+fi
 
 mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$1
