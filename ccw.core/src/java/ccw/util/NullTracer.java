b24c5265452abc4af5fd9836dde2efb506755b07
Laurent Petit <laurent.petit@gmail.com>
2013-12-12 21:41:05 +0100

    fix(Tracing): better traced message layout for NullTracer

--- ccw.core/src/java/ccw/util/NullTracer.java
+++ ccw.core/src/java/ccw/util/NullTracer.java
@@ -11,19 +11,19 @@ public final class NullTracer implements ITracer {
     public boolean isEnabled(String traceOption) { return true; }
 
     public void trace(String traceOption, Object... message) {
-    	System.out.println("trace[traceOption:" + traceOption + ", message:"
-    			+ Tracer.buildMessage(message) + "]");
+    	System.out.println("trace:" + traceOption + ", message:"
+    			+ Tracer.buildMessage(message));
     }
 
     public void trace(String traceOption, Throwable throwable, Object... message) {
-    	System.out.println("trace[traceOption:" + traceOption + ", message:"
-    			+ Tracer.buildMessage(message) + "]");
+    	System.out.println("trace:" + traceOption + ", message:"
+    			+ Tracer.buildMessage(message));
     	throwable.printStackTrace();
     }
 
     public void traceDumpStack(String traceOption) {
     	try {
-    		throw new RuntimeException("traceDumpStack[traceOption:" + traceOption + "]");
+    		throw new RuntimeException("traceDumpStack:" + traceOption);
     	} catch (RuntimeException e) {
     		e.printStackTrace();
     	}
@@ -31,7 +31,7 @@ public final class NullTracer implements ITracer {
 
     public void traceEntry(String traceOption) {
     	try {
-    		throw new RuntimeException("traceEntry[traceOption:" + traceOption + "]");
+    		throw new RuntimeException("traceEntry:" + traceOption);
     	} catch (RuntimeException e) {
     		e.printStackTrace();
     	}
@@ -39,8 +39,8 @@ public final class NullTracer implements ITracer {
 
     public void traceEntry(String traceOption, Object... arguments) {
     	try {
-    		throw new RuntimeException("traceEntry[traceOption:" + traceOption
-    				+ ", arguments:" + Tracer.buildMessage(arguments) + "]");
+    		throw new RuntimeException("traceEntry:" + traceOption
+    				+ ", arguments:" + Tracer.buildMessage(arguments));
     	} catch (RuntimeException e) {
     		e.printStackTrace();
     	}
@@ -48,7 +48,7 @@ public final class NullTracer implements ITracer {
 
     public void traceExit(String traceOption) {
     	try {
-    		throw new RuntimeException("traceExit[traceOption:" + traceOption + "]");
+    		throw new RuntimeException("traceExit:" + traceOption);
     	} catch (RuntimeException e) {
     		e.printStackTrace();
     	}
@@ -56,8 +56,8 @@ public final class NullTracer implements ITracer {
 
     public void traceExit(String traceOption, Object returnValue) {
     	try {
-    		throw new RuntimeException("traceExit[traceOption:" + traceOption 
-    				+ ", returnValue:" + returnValue + "]");
+    		throw new RuntimeException("traceExit:" + traceOption 
+    				+ ", returnValue:" + returnValue);
     	} catch (RuntimeException e) {
     		e.printStackTrace();
     	}
