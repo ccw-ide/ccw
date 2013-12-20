5c3b1c74fbc855b90ddcecfe0588cfb258a70411
Laurent Petit <laurent.petit@gmail.com>
2013-12-20 18:58:55 +0100

    feat(repl): Upgraded the nrepl version from 0.2.1 to 0.2.3

--- ccw.core/project.clj
+++ ccw.core/project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [leiningen "2.1.2"]
-                 [org.clojure/tools.nrepl "0.2.1"]
+                 [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/core.incubator "0.1.0"]
                  [com.cemerick/drawbridge "0.0.6"]
                  [net.cgrand/parsley "0.9.1"]
