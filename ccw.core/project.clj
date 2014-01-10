d27547b5e70bdaf6a769961d93d92df008bd14d0
Laurent Petit <laurent.petit@gmail.com>
2014-01-10 11:35:08 +0100

    feat(lein): upgraded the embedded Leiningen version: 2.1.2 -> patched 2.3.4
    Leiningen 2.3.4 applied patches:
    - nREPL URL appended to default message notifying user of nREPL host/port

--- ccw.core/project.clj
+++ ccw.core/project.clj
@@ -4,7 +4,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [leiningen "2.1.2"]
+                 [leiningen "2.3.4"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/core.incubator "0.1.0"]
                  [com.cemerick/drawbridge "0.0.6"]
