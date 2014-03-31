7682c3e594788d0d8eb216021c029069939c7819
François Rey <git_@_francois_._rey_._name>
2014-03-31 12:11:28 +0200

    make-factory works and also supports instantiation from given bundle.

--- ccw.core/src/clj/ccw/core/factories.clj
+++ ccw.core/src/clj/ccw/core/factories.clj
@@ -13,18 +13,23 @@
              []
         (execute [event] (when handler (handler this event)))))))
 
-(defn java-factory 
-  "Factory for an Eclipse plugin executable extension which calls the no-arg
-   constructor of the java class whose fully qualified name is passed in javaClass"
-  [{java-class "javaClass"}]
-  (println "I was called, hey!")
-  (try
-    (let [class (-> (Thread/currentThread) 
-                  .getContextClassLoader
-                  (.loadClass java-class))]
-      (let [i (Reflector/invokeConstructor class (make-array Object 0))]
-        (println "instance created: " i)
-        i))
-    (catch Exception e
-      (println "sent exception: " (.getMessage e) "," e)
-      (.printStackTrace e))))
+(defn make-factory
+  "Creates a factory function for creating instances of the given java
+   class name using the constructor with given argument types.
+   The types of the arguments must be specified in a symbol sequence,
+   which must be emtpy when the no arg constructor is to be used.
+   The fn produced uses type hints to enable compiler/jit
+   optimizations, at the expense of making this factory method slower
+   so it's best to store its result in a var.
+   A Bundle instance can optionally be passed for loading the class.
+   Example:
+     (def int-factory (make-factory \"Integer\" ['String]))
+     (int-factory \"42\")
+   See http://stackoverflow.com/a/3752276 for original inspiration."
+  ([classname types]
+    (let [args (map #(with-meta (symbol (str "x" %2)) {:tag %1}) types (range)) ]
+      (eval `(fn [~@args] (new ~(symbol classname) ~@args)))))
+  ([bundle classname types]
+    (let [zclass (.. bundle (loadClass classname))
+          constructor (.getConstructor zclass (into-array Class (map resolve types)))]
+      (fn [& args] (.newInstance constructor (into-array Object args))))))
