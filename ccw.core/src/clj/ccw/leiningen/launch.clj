94b1ec2714c8ab8d846ea2add7c4b027eff166ef
Laurent Petit <laurent.petit@gmail.com>
2014-04-24 12:11:30 +0200

    feat(launch): improve lein launch by adding JVM options as does Leiningen

--- ccw.core/src/clj/ccw/leiningen/launch.clj
+++ ccw.core/src/clj/ccw/leiningen/launch.clj
@@ -7,26 +7,31 @@
   ([project command] 
     (lein-launch-configuration project command nil))
   ([project command {:keys [leiningen-standalone-path launch-name] :as options}]
-    {
-     :private                true
-     :launch-in-background   false
-     :append-environment-variables true
-     :name                   launch-name
-     
-     :java/project-name      (and project (e/project-name project))
-     :java/classpath         [{:entry-type :lib
-                               :path (or 
-                                       leiningen-standalone-path
-                                       (e/get-file-inside-plugin "ccw.core" "leiningen-standalone.jar")) }
-                              {:entry-type :lib
-                               :path (e/get-file-inside-plugin "ccw.core" "lein")}
-                              {:entry-type :jre-container
-                               :name launch/default-jre-container-name}]
-     :java/default-classpath false
-     :java/vm-arguments      (str " -Dfile.encoding=UTF-8"
-                                  " -Dmaven.wagon.http.ssl.easy=false")
-     :java/main-type-name    "clojure.main"
-     :java/program-arguments (str "-m ccw.leiningen.main " command)}))
+    (let [leiningen-standalone-path (or leiningen-standalone-path
+                                        (e/get-file-inside-plugin "ccw.core" "leiningen-standalone.jar"))]
+      {
+       :private                true
+       :launch-in-background   false
+       :append-environment-variables true
+       :name                   launch-name
+       
+       :java/project-name      (and project (e/project-name project))
+       :java/classpath         [{:entry-type :lib
+                                 :path leiningen-standalone-path}
+                                {:entry-type :lib
+                                 :path (e/get-file-inside-plugin "ccw.core" "lein")}
+                                {:entry-type :jre-container
+                                 :name launch/default-jre-container-name}]
+       :java/default-classpath false
+       :java/vm-arguments      (str 
+                                 " -client" 
+                                 " -Xbootclasspath/a:" (.toOSString (e/path leiningen-standalone-path)) 
+                                 " -XX:+TieredCompilation"
+                                 " -XX:TieredStopAtLevel=1"
+                                 " -Dfile.encoding=UTF-8"
+                                 " -Dmaven.wagon.http.ssl.easy=false")
+       :java/main-type-name    "clojure.main"
+       :java/program-arguments (str "-m ccw.leiningen.main " command)})))
 
 (defn lein 
   "project can be nil"
