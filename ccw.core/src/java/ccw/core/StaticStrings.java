99608dbb6709c511b615d6dae232ec5bfeed1f3c
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-01-12 20:25:53 +0100

    feat(repl): now you can enable the cider (default) middleware with -Dccw.nrepl.cider.enable
    This feature allows to have the cider middleware and therefore full cider support on the embedded
    Counterclockwise nRepl. Without it, a warning is raised and "apropos classpath complete eldoc..."
    won't be working. The property ccw.nrepl.cider.enable needs to be present at runtime.
    
    The new functionality is enabled by adding -Dccw.nrepl.cider.enable to the command line or in
    Counterclockwise.ini.
    
    Conflicts:
    	ccw.core/src/clj/ccw/core/launch.clj

--- ccw.core/src/java/ccw/core/StaticStrings.java
+++ ccw.core/src/java/ccw/core/StaticStrings.java
@@ -11,5 +11,6 @@ public class StaticStrings {
 	// Properties
 	public static final String CCW_PROPERTY_NREPL_PORT = "ccw.nrepl.port";
 	public static final String CCW_PROPERTY_NREPL_AUTOSTART = "ccw.nrepl.autostart";
+	public static final String CCW_PROPERTY_NREPL_CIDER_ENABLE = "ccw.nrepl.cider.enable";
 	
 }
