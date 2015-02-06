b077c8958676ba0340db67c6efeb429de7da922c
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-02-06 15:41:47 +0100

    feat(ccw.core-hovers): added support for ITextHoverExtension2 to docstring_hover
    Adds ITextHoverExtension2 and getHoverInfo2 for Docstring reified instances.
    
    The old getHoverInfo has been deprecated and always returns null.

--- ccw.core/src/clj/ccw/editors/clojure/hovers/docstring_hover.clj
+++ ccw.core/src/clj/ccw/editors/clojure/hovers/docstring_hover.clj
@@ -1,7 +1,8 @@
 (ns ccw.editors.clojure.hovers.docstring-hover
   "Supports documentation hovers for Clojure Editor"
-  (:import [org.eclipse.jface.text Region ITextHover]
-           ccw.editors.clojure.IClojureAwarePart
+  (:import [org.eclipse.jface.text Region ITextHover
+                                          ITextHoverExtension2]
+           ccw.editors.clojure.IClojureSourceViewer
            ccw.CCWPlugin)
   (:require [ccw.core.trace :refer [trace]]
             [ccw.interop :refer [simple-name]]
@@ -15,13 +16,16 @@
 (defn- hover-info
   "Return the documentation hover text to be displayed at offset offset for
   editor. The text can be composed of a subset of html (e.g. <pre>, <i>, etc.)"
-  [^IClojureAwarePart editor offset]
-  (let [loc (offset-loc editor offset)
+  [^IClojureSourceViewer text-viewer offset]
+  (let [loc (offset-loc text-viewer offset)
         parse-symbol (parse-symbol? loc)]
+    (trace :support/hover (str "[DOCSTRING-HOVER] hover-info:\n"
+                               "offset -> " offset "\n"
+                               "parse-symbol -> " parse-symbol "\n"))
     (when parse-symbol
       (let [m (find-var-metadata
-               (.findDeclaringNamespace editor)
-               editor
+               (.findDeclaringNamespace text-viewer)
+               text-viewer
                parse-symbol)]
         (var-doc-info-html m)))))
 
@@ -40,15 +44,23 @@
   "Factory function for creating an ITextHover instance for the editor."
   []
   (reify
-    ITextHover
-    (getHoverInfo [this text-viewer hover-region]
-      (trace :support/hover (str "[DOCSTRING-HOVER] " (simple-name this) ".getHoverInfo called with:\n"
+    ITextHoverExtension2
+    (getHoverInfo2 [this text-viewer hover-region]
+      (trace :support/hover (str "[DOCSTRING-HOVER] " (simple-name this) ".getHoverInfo2 called:\n"
                                  "text-viewer -> " (.toString text-viewer) "\n"
                                  "region -> " (.toString hover-region) "\n"))
       (hover-info text-viewer (.getOffset hover-region)))
 
+    ITextHover
+    (getHoverInfo [this text-viewer hover-region]
+      (trace :support/hover (str "[DOCSTRING-HOVER] " (simple-name this) ".getHoverInfo called:\n"
+                                 "text-viewer -> " (.toString text-viewer) "\n"
+                                 "region -> " (.toString hover-region) "\n"))
+      ;; AR - Deprecated (hover-info text-viewer (.getOffset hover-region))
+      nil)
+
     (getHoverRegion [this text-viewer offset]
-      (trace :support/hover (str "[DOCSTRING-HOVER] " (simple-name this) ".getHoverRegion called with:\n"
+      (trace :support/hover (str "[DOCSTRING-HOVER] " (simple-name this) ".getHoverRegion called:\n"
                                  "text-viewer -> "(.toString text-viewer) "\n"
                                  "offset -> " offset "\n"))
       (let [[offset length] (hover-region text-viewer offset)]
