53d5d2f0832a51e674facdecfedc570b0301a2d3
Laurent Petit <laurent.petit@gmail.com>
2015-04-12 00:26:10 +0200

    feat(plugin): display maven coordinates of embedded dependencies
    Fixes #741

--- ccw.core/src/java/ccw/CCWPlugin.java
+++ ccw.core/src/java/ccw/CCWPlugin.java
@@ -5,14 +5,24 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors: 
+ * Contributors:
  *    Casey Marshall  - initial API and implementation
  *    Thomas Ettinger - paren matching generic code
  *    Stephan Muehlstrasser - preference support for syntax coloring
  *******************************************************************************/
 package ccw;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
+import java.util.TreeSet;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.eclipse.core.resources.IProject;
@@ -66,37 +76,37 @@ public class CCWPlugin extends AbstractUIPlugin {
 
     /** The plug-in ID */
     public static final String PLUGIN_ID = "ccw.core";
-    
+
     /** Leiningen project nature */
     public static final String LEININGEN_NATURE_ID = "ccw.leiningen.nature";
 
-    /** 
+    /**
      * @param swtKey a key from SWT.COLOR_xxx
-     * @return a system managed color (callers must not dispose 
+     * @return a system managed color (callers must not dispose
      *         the color themselves)
      */
-	public static Color getSystemColor(int swtKey) { 
-		return Display.getDefault().getSystemColor(swtKey); 
+	public static Color getSystemColor(int swtKey) {
+		return Display.getDefault().getSystemColor(swtKey);
 	}
-	
-	
+
+
 	//SHOULD LOG INFO / WARN / ERROR WHEN THE APPROPRIATE FLAGS ARE SET SO THAT ONE DOES NOT HAVE
 	//TO GO FROM ONE FILE TO ANOTHER
-	//ALSO CONSIDER VARIANTS FOR STACKTRACE, 
+	//ALSO CONSIDER VARIANTS FOR STACKTRACE,
 	//RAW STRING (no format), etc.
-	
+
     /** The shared instance */
     private static CCWPlugin plugin;
 
     private ColorRegistry colorCache;
-    
+
     private FontRegistry fontRegistry;
-    
-    
+
+
 	private AutomaticNatureAdder synchronizedNatureAdapter;
 
 	private ITracer tracer = NullTracer.INSTANCE;
-	
+
 	public static ITracer getTracer() {
 		CCWPlugin plugin = getDefault();
 		if (plugin != null && plugin.tracer != null)
@@ -104,7 +114,7 @@ public class CCWPlugin extends AbstractUIPlugin {
 		else
 			return NullTracer.INSTANCE;
 	}
-    
+
     public void startREPLServer() {
     	try {
     	    ClojureInvoker.newInvoker(this, "ccw.core.launch")._("ccw-nrepl-start-if-necessary");
@@ -113,30 +123,128 @@ public class CCWPlugin extends AbstractUIPlugin {
     		throw new RuntimeException("Could not start plugin-hosted REPL server", e);
     	}
     }
-    
+
     public int getREPLServerPort() {
     	startREPLServer();
     	return (Integer) ClojureInvoker.newInvoker(this, "ccw.core.launch")._("ccw-nrepl-port");
     }
 
-    public CCWPlugin() {
-    	System.out.println("CCWPlugin instanciated");
+    /**
+     * Record of a found CCW dependency. Only used at plugin startup to
+     * log found dependencies.
+     * @see logDependenciesInformation(final BundleContext context)
+     */
+    private static class CCWDependency {
+    	public final String groupId;
+    	public final String artifactId;
+    	public final String version;
+    	public CCWDependency(String g, String a, String v) {
+    		groupId = g; artifactId = a; version = v;
+    	}
     }
-    
-    public void start(BundleContext context) throws Exception {
+    /**
+     * Record of a found CCW dependency in error. Only used at plugin startup to
+     * log found dependencies.
+     * @see logDependenciesInformation(final BundleContext context)
+     */
+    private static class CCWDependencyErr {
+    	public final URL url;
+    	public final Exception exception;
+    	public CCWDependencyErr(URL u, Exception e) {
+    		url = u; exception = e;
+    	}
+    }
+
+    /**
+     * Method implemented in java so we don't trigger clojure accidentally,
+     * because this method is called really early in the plugin initialization
+     * process
+     */
+    private final void logDependenciesInformation(final BundleContext context) {
+
+    	// Collect bundle embedded external libraries
+    	Enumeration<URL> entries = context.getBundle().findEntries("lib", "pom.properties", true);
+
+    	// Initialize colls for dependencies and dependencies in error
+    	// dependencies will be sorted first via groupId, then artifactId
+    	TreeSet<CCWDependency> deps = new TreeSet<CCWDependency>(new Comparator<CCWDependency>() {
+			@Override public int compare(CCWDependency d1, CCWDependency d2) {
+				if (d1.groupId.equals(d2.groupId)) {
+					return d1.artifactId.compareTo(d2.artifactId);
+				} else {
+					return d1.groupId.compareTo(d2.groupId);
+				}
+			}
+		});
+    	// errors are collected without specific ordering
+		List<CCWDependencyErr> errs = new ArrayList<CCWDependencyErr>();
+
+		// Collect dependencies and dependencies in error, gathering as much
+		// info as possible
+    	if (entries != null) {
+    		while (entries.hasMoreElements()) {
+    			URL entry = entries.nextElement();
+    			Reader r = null;
+    			try {
+	    			r = new InputStreamReader(entry.openStream(), Charset.forName("UTF-8"));
+	    			Properties p = new Properties();
+	    			p.load(r);
+	    			String groupId = p.getProperty("groupId");
+	    			String artifactId = p.getProperty("artifactId");
+	    			String version = p.getProperty("version");
+	    			CCWDependency d = new CCWDependency(groupId, artifactId, version);
+	    			deps.add(d);
+    			} catch (IOException e) {
+    				errs.add(new CCWDependencyErr(entry, e));
+    			} finally {
+    				if (r != null)
+    					try { r.close(); } catch (IOException e) { /* silently ignore */ }
+    			}
+    		}
+    	}
+
+    	// Create the dependencies report String and log it
+		StringBuilder sb = new StringBuilder();
+		sb.append("Counterclockwise dependencies:");
+		final String indent = "        ";
+    	if (entries != null) {
+    		for (CCWDependencyErr err: errs) {
+    			logWarning("Exception while trying to find maven information for Counterclockwise dependency " + err.url.toExternalForm(), err.exception);
+    			sb.append("\n" + indent);
+				sb.append(err.url.toExternalForm() + ": " + err.exception.getMessage());
+    		}
+    		for (CCWDependency d: deps) {
+    			String name = (d.groupId.equals(d.artifactId)) ? d.groupId : d.groupId + "/" + d.artifactId;
+    			sb.append("\n" + indent);
+    			sb.append(String.format("[%s \"%s\"]", name, d.version));
+    		}
+    	} else {
+    		sb.append(indent + "no entries found in bundle");
+    	}
+    	// We do not call CCWPlugin.log() because it would duplicate the log
+    	// in .metadata/.log because at the time it's called, it would not find
+    	// the trace() machinery already installed
+    	plugin.getLog().log(new Status(IStatus.INFO, PLUGIN_ID, sb.toString()));
+    }
+
+    public CCWPlugin() { }
+
+    @Override
+	public void start(BundleContext context) throws Exception {
         super.start(context);
         System.out.println("CCWPlugin.start: ENTER");
         plugin = this;
+        logDependenciesInformation(context);
 
         context.addBundleListener(new BundleListener() {
-			
+
 			@Override
 			public void bundleChanged(BundleEvent evt) {
 				if (evt.getBundle() == CCWPlugin.this.getBundle()
 					&&	evt.getType() == BundleEvent.STARTED) {
-					
+
 					tracer = new Tracer(evt.getBundle().getBundleContext(), TraceOptions.getTraceOptions());
-					
+
 					// We immediately give control back to the OSGi framework application
 					// by starting the code in a new thread
 					new Thread(new Runnable() {
