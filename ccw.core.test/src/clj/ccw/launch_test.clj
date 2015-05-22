20511d811cc685a3970e18df6ac31f8a01f40256
Laurent Petit <laurent.petit@gmail.com>
2015-05-22 16:27:41 +0200

    test(clojure): more independent test launch configuration

--- ccw.core.test/src/clj/ccw/launch_test.clj
+++ ccw.core.test/src/clj/ccw/launch_test.clj
@@ -12,7 +12,7 @@
 (deftest test-lein-launch-configuration
   (testing "lein-launch-configuration"
            (testing "returns quoted bootclasspath with fixed path"
-                    (let [config (lein-launch-configuration new-project "some-command" {:leiningen-standalone-path "some.jar"})]
+                    (let [config (lein-launch-configuration (new-project) "some-command" {:leiningen-standalone-path "some.jar"})]
                       (is (map? config))
                       (let [matches (re-seq #"-Xbootclasspath/a:([^\s]*)" (:java/vm-arguments config))]
                         (let [bootclasspath (second (first matches))]
@@ -22,7 +22,7 @@
                         )
                       ))
            (testing "returns quoted bootclasspath without fixed path"
-                    (let [config (lein-launch-configuration new-project "some-command")]
+                    (let [config (lein-launch-configuration (new-project) "some-command")]
                       (is (map? config))
                       (let [matches (re-seq #"-Xbootclasspath/a:([^\s]*)" (:java/vm-arguments config))]
                         (let [bootclasspath (second (first matches))]
