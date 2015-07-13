ebfde493d354b1bdaa083e06e2356c5e9f538e6a
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-07-13 18:58:03 +0200

    feat(ccw.core.test): add Test and Generator... menu and fixtures for it
    This patch enables the user to record tests using the SWTBot generator and therefore ease the pain
    of bug reporting. It is also useful for debugging purposes and/or test enhancing.
    A contributed menu entry called "Generator..." in the Test menu will open the SWTBot interface. In
    the process, the MANIFEST.MF has been updated with the new version of SWTBot and updates it with the
    dependencies taken from the SWTBot samples. Hamcrest imports were also needed. Tests were added.
    
    Note1: the Clojure menu is always shown now because the user might want to call Test Generator even
    when the active part is not the ClojureEditor.
    Note2: the patch also performs a little refactoring of the command contributions.

--- ccw.core.test/src/java/ccw/core/SmokeTests.java
+++ ccw.core.test/src/java/ccw/core/SmokeTests.java
@@ -1,8 +1,13 @@
 package ccw.core;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
 
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swtbot.swt.finder.SWTBotAssert;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.utils.SWTUtils;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -36,5 +41,20 @@ public class SmokeTests {
 		.createClojureProject("my-first-clojure-project")
 		.assertProjectExists("my-first-clojure-project");
 	}
- 
+	
+	@Test
+    public void canAlwaysShowClojureMenu() throws Exception {
+	    SWTBotAssert.assertVisible(bot.menu("Clojure"));
+    }
+
+    @Test
+    public void swtbotDoesNotRunOnTheUIThread() throws Exception {
+        assertNull(Display.getCurrent());
+        assertNotSame(Thread.currentThread(), SWTUtils.display().getThread());
+    }
+
+    @Test
+    public void canShowTestGeneratorEntryInClojureMenu() throws Exception {
+        SWTBotAssert.assertVisible(bot.menu("Clojure", "Test", "Generator..."));
+    }
 }
