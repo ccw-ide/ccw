c9996239139c701630d870e5ea3746440ee3c461
Laurent Petit <laurent.petit@gmail.com>
2016-07-06 23:24:50 +0200

    feat(swtbot): back to swtbot generator feature.

--- ccw.core/src/java/ccw/editors/clojure/TestGeneratorHandler.java
+++ ccw.core/src/java/ccw/editors/clojure/TestGeneratorHandler.java
@@ -3,12 +3,12 @@ package ccw.editors.clojure;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-//import org.eclipse.swtbot.generator.ui.StartupRecorder;
+import org.eclipse.swtbot.generator.ui.StartupRecorder;
 
 public class TestGeneratorHandler extends AbstractHandler {
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-//        StartupRecorder.openRecorder(null);
+        StartupRecorder.openRecorder(null);
         return null;
     }
 }
